9a8150c2c2b9214e903c07f0e6920302eda779c6
Kraus <fkraus@etfos.hr>
2016-07-09 18:01:16 +0200

    testovi

--- projekt/stranica/index.php
+++ projekt/stranica/index.php
@@ -1,2 +1,13 @@
 <?php
 
+$query= "SELECT * FROM 1354734_web.korisnik WHERE 1";
+    if (!($q=@mysql_query($query)) && !$logerr)
+        $logerr = "Neuspjelo slanje upita bazi!";
+    if (@mysql_num_rows($q)==0 && !$logerr)
+      $logerr = "Prazan red!";
+    else if(!$logerr){
+      while(($redak = @mysql_fetch_array($q) )) 
+		  if($redak['nick']){
+			echo '';
+			}
+	}
